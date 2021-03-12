c74bab209b8a5822a911cbdb9ced05a74b7208a4
pittcat <pitt_wenchao@outlook.com>
2021-03-13 01:51:06 +0800

    update ndap for go and js

--- layers/tools/ndap/ndap.lua
+++ layers/tools/ndap/ndap.lua
@@ -12,6 +12,58 @@ table.insert(dap.configurations.python, {
   console = 'externalTerminal';
 })
 
+dap.adapters.go = function(callback, config)
+  local handle
+  local pid_or_err
+  local port = 38697
+  handle, pid_or_err =
+    vim.loop.spawn(
+    "dlv",
+    {
+      args = {"dap", "-l", "127.0.0.1:" .. port},
+      detached = true
+    },
+    function(code)
+      handle:close()
+      print("Delve exited with exit code: " .. code)
+    end
+  )
+   ----should we wait for delve to start???
+  vim.defer_fn(
+  function()
+    dap.repl.open()
+    callback({type = "server", host = "127.0.0.1", port = port})
+  end,
+  100)
+end
+-- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
+dap.configurations.go = {
+  {
+    type = "go",
+    name = "Debug",
+    request = "launch",
+    program = "${file}",
+  }
+}
+
+
+-- js
+dap.adapters.node2 = {
+  type = 'executable',
+  command = 'node',
+  args = {os.getenv('HOME') .. '/.ndap_debugger/vscode-node-debug2/out/src/nodeDebug.js'},
+}
+dap.configurations.javascript = {
+  {
+    type = 'node2',
+    request = 'launch',
+    program = '${file}',
+    cwd = vim.fn.getcwd(),
+    sourceMaps = true,
+    protocol = 'inspector',
+    console = 'integratedTerminal',
+  },
+}
 
 -- external terminal
 dap.defaults.fallback.external_terminal = {
